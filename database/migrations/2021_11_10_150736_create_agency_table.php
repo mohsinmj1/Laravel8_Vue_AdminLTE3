a669f8b078efa4abf8c9aee76e38f1bc4a079a8c
Muhammad Zain Ali <zain.ali@bnu.edu.pk>
2021-11-11 23:10:47 +0500

    agency

---
+++ database/migrations/2021_11_10_150736_create_agency_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agency', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('placement_id');
+            $table->string('name');
+            $table->string('department');
+            $table->string('email');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('phone');
+            $table->string('mobile');
+            $table->string('address');
+            $table->string('Country');
+            $table->string('country_code');
+            $table->string('town');
+            $table->string('postcode');
+            $table->string('logo');
+            $table->string('favicon');
+            $table->string('ic_department');
+            $table->string('ic_email');
+            $table->string('ic_fname');
+            $table->string('ic_lname');
+            $table->string('ic_phone');
+            $table->string('ic_mobile');
+            $table->string('ic_address');
+            $table->string('ic_Country');
+            $table->string('ic_country_code');
+            $table->string('ic_town');
+            $table->string('ic_postcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agency');
+    }
+}
